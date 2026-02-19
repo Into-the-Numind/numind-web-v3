7ce1d75db404f215cecd3d5b804ba5ebc6f7a94e
pmtmyaggy <pmtmyaggy@gmail.com>
2026-02-19 23:43:03 +0800

    修复前端映射问题

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,17 @@
 set -eu
 
 # 按环境注入 API 反向代理上游地址
-API_PROXY_PASS="${API_PROXY_PASS:-http://host.docker.internal:9091/}"
+API_PROXY_PASS="${API_PROXY_PASS:-http://127.0.0.1:9091/}"
 case "$API_PROXY_PASS" in
   */) ;;
   *) API_PROXY_PASS="${API_PROXY_PASS}/" ;;
 esac
 export API_PROXY_PASS
 
-envsubst '${API_PROXY_PASS}' \
-  < /etc/nginx/templates/default.conf.template \
-  > /etc/nginx/conf.d/default.conf
+if [ -f /etc/nginx/templates/default.conf.template ]; then
+  envsubst '${API_PROXY_PASS}' \
+    < /etc/nginx/templates/default.conf.template \
+    > /etc/nginx/conf.d/default.conf
+fi
 
 exec "$@"
